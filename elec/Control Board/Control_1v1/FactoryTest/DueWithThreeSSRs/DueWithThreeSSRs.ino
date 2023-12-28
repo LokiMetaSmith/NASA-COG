3016e2ee583e10fcc961cf685eadb18521bee440
lokimetasmith <lokimail@gmail.com>
2023-12-28 14:37:28 -0600

    added power supply, compiles but doesn't completely work
    Signed-off-by: lokimetasmith <lokimail@gmail.com>

--- elec/Control Board/Control_1v1/FactoryTest/DueWithThreeSSRs/DueWithThreeSSRs.ino
+++ elec/Control Board/Control_1v1/FactoryTest/DueWithThreeSSRs/DueWithThreeSSRs.ino
@@ -16,7 +16,7 @@
 
 #define COMPANY_NAME "pubinv.org "
 #define PROG_NAME "OEDCS_Factory_Test"
-#define VERSION ";_Rev_0.5"
+#define VERSION ";_Rev_0.5.5"
 #define DEVICE_UNDER_TEST "Hardware:_Control_V1.1"  //A model number
 #define LICENSE "GNU Affero General Public License, version 3 "
 
@@ -65,7 +65,7 @@ class PowerSense
     // Constructor - creates a Flasher
     // and initializes the member variables and state
   public:
-    PowerSense(const String pinName, int pin, long period, float R1 = 30000, float R2 = 4700, int offsetX = 0, int offsetY = 0)
+    PowerSense(const String pinName, int pin, long period, float R1 = 40000, float R2 = 4700, int offsetX = 0, int offsetY = 0)
     {
       ADCinPin = pin;
       previousMillis = 0;
@@ -246,11 +246,13 @@ bool updatePowerMonitor(void) {
   // SENSE_24V on A1.
   // Full scale is 1023, ten bits for 3.3V.
   //30K into 4K7
-  const long R1 = 30000;
+  const long FullScale = 1023;
+  const float percentOK = 0.75;
+  const long R1 = 40000;
   const long R2 = 4700;
   const float Vcc = 3.3;
   bool powerIsGood = false;
-  int lowThreshold24V = 1023 * 3 / 4;
+  int lowThreshold24V = (24*(R2/(R1+R2))/Vcc)*FullScale *percentOK;  //1023 * 3 / 4;
 
   if (analogRead(A1) > lowThreshold24V) {
     powerIsGood = true;
@@ -261,14 +263,318 @@ bool updatePowerMonitor(void) {
   }
 }
 
+// #define ADDRESS 0x04
+#define ADDRESS 0x00
+#define MYDELAY 500
+uint8_t error = 0;// error
+char manuf[17]; // INFO 0
+char model[17];  // INFO 1
+char voltage_string[5]; // INFO 2
+char revision[5]; // INFO 3
+char manuf_date[9];  // INFO 4
+char serial[17];  // INFO 5
+char country[17];  // INFO 6
+uint16_t rate_voltage;  //50-51
+uint16_t rate_current;  //52-53
+uint16_t max_voltage;  //54-55
+uint16_t max_current;  //56-57
+uint16_t out_voltage;  //60-61
+uint16_t out_current;  //62-63
+uint8_t temp;  //68
+uint8_t status0;  //6C
+uint8_t status1;  //6F
+uint16_t set_voltage; // 70-71 r/w
+uint16_t set_current; // 72-73 r/w
+uint8_t control; //  7C  r/w
+uint8_t on_off;
+String serial1Buffer;
+
+int setPS_Addr(uint8_t addr) {
+  Serial1.print("ADDS "); Serial1.print(addr); Serial1.print("\r\n");
+  delay(50);
+  bool validEntry = true;
+  char buff[5];
+ 
+  uint8_t c = Serial1.readBytesUntil('\n', buff, sizeof buff);
+  while( validEntry)
+  if (c != 3 || buff[0] != '=' || buff[1] != '>') return 0;
+  return 1;
+}
+
+int setPS_Val(uint8_t addr, const char *loc, const char *val) {
+  if (!setPS_Addr(addr)) {
+    Serial.println("didn't set address");
+    return 0;
+  }
+
+  Serial1.print(loc); Serial1.print(' '); Serial1.print(val); Serial1.print("\r\n");
+  delay(100);
+  char b[5];
+  int c = Serial1.readBytesUntil('\n', b, sizeof b);
+  if (c != 3 || b[0] != '=' || b[1] != '>') return 0;
+  return 1;
+}
+
+int setPS_GlobOnOff(uint8_t addr, const char *val) {
+  if (strcasecmp(val, "on") == 0) val = "1";
+  else val = "0";
+  return setPS_Val(addr, "GLOB", val);
+}
+
+int setPS_OnOff(uint8_t addr, const char *val) {
+  if (strcasecmp(val, "on") == 0) val = "1";
+  else val = "0";
+  return setPS_Val(addr, "POWER", val);
+}
+
+int setPS_Voltage(uint8_t addr, uint16_t volts) {
+  char b[7];
+  snprintf(b, sizeof b, "%4.1f", volts / 100.0);
+  return setPS_Val(addr, "SV", b);
+}
+
+int setPS_Current(uint8_t addr, uint16_t amps) {
+  char b[7];
+  snprintf(b, sizeof b, "%4.1f", amps / 100.0);
+  return setPS_Val(addr, "SI", b);
+}
+
+// TODO: This is untested
+int setPS_GCurrent(uint8_t addr, uint16_t amps) {
+  char b[7];
+  snprintf(b, sizeof b, "%4.1f", amps / 100.0);
+  return setPS_Val(addr, "GSI", b);
+}
+
+char *getPS_Val(uint8_t addr, const char *val) {
+  static char rval[50];
+  if (!setPS_Addr(addr)) {
+    Serial.println("didn't set address");
+    return 0;
+  }
+
+  Serial1.print(val); Serial1.print("\r\n");
+  delay(100);
+  int c = Serial1.readBytesUntil('\n', rval, sizeof rval);
+  rval[c-1] = '\0';
+  delay(10);
+  char b[5];
+  c = Serial1.readBytesUntil('\n', b, sizeof b);
+  if (c != 3 || b[0] != '=' || b[1] != '>') return NULL;
+  return rval;
+}
+
+void getPS_Manuf(int addr) {
+  char *r = getPS_Val(addr, "INFO 0");
+  strncpy(manuf, r, sizeof manuf);
+}
+
+void getPS_Model(int addr) {
+  char *r = getPS_Val(addr, "INFO 1");
+  strncpy(model, r, sizeof model);
+}
+
+void getPS_VoltageString(int addr) {
+  char *r = getPS_Val(addr, "INFO 2");
+  strncpy(voltage_string, r, sizeof voltage_string);
+}
+
+void getPS_Revision(int addr) {
+  char *r = getPS_Val(addr, "INFO 3");
+  strncpy(revision, r, sizeof revision);
+}
+
+void getPS_ManufDate(int addr) {
+  char *r = getPS_Val(addr, "INFO 4");
+  strncpy(manuf_date, r, sizeof manuf_date);
+}
+
+void getPS_Serial(int addr) {
+  char *r = getPS_Val(addr, "INFO 5");
+  strncpy(serial, r, sizeof serial);
+}
+
+void getPS_Country(int addr) {
+  char *r = getPS_Val(addr, "INFO 6");
+  strncpy(country, r, sizeof country);
+}
+
+void getPS_RateVoltage(int addr) {
+  char *r = getPS_Val(addr, "RATE?");
+  char b[20];
+  strncpy(b, r, sizeof b);
+  char *ptr = NULL;
+  rate_voltage = -1;
+  if ((ptr = strchr(b, ' '))) {
+    *ptr = '\0';
+    rate_voltage = int(atof(b) * 100);
+  }
+}
+
+void getPS_RateCurrent(int addr) {
+  char *r = getPS_Val(addr, "RATE?");
+  char b[20];
+  strncpy(b, r, sizeof b);
+  char *ptr = NULL;
+  rate_current = -1;
+  if ((ptr = strchr(b, ' '))) {
+    rate_current = int(atof(ptr + 1) * 100);
+  }
+}
+
+void getPS_OnOff(int addr) {
+  char *r = getPS_Val(addr, "POWER 2");
+  switch (r[0]) {
+  case '0': on_off = 0; break;
+  case '1': on_off = 1; break;
+  case '2': on_off = 0; break;
+  case '3': on_off = 1; break;
+  }
+}
+
+void getPS_MaxVoltage(int addr) {
+  max_voltage = -1;
+}
+
+void getPS_MaxCurrent(int addr) {
+  max_current = -1;
+}
+
+void getPS_OutVoltage(int addr) {
+  char *r = getPS_Val(addr, "RV?");
+  out_voltage = int(atof(r) * 100);
+}
+
+void getPS_OutCurrent(int addr) {
+  char *r = getPS_Val(addr, "RI?");
+  out_current = int(atof(r) * 100);
+}
+
+void getPS_Status0(int addr) {
+  char *r = getPS_Val(addr, "STUS 0");
+  status0 = (r[0] - '0') << 4;
+  status0 += (r[1] - '0') & 0x0F;
+}
+
+void getPS_Status1(int addr) {
+  char *r = getPS_Val(addr, "STUS 1");
+  status1 = (r[0] - '0') << 4;
+  status1 += (r[1] - '0') & 0x0F;
+}
+
+void getPS_Temp(int addr) {
+  char *r = getPS_Val(addr, "RT?");
+  temp = atoi(r);
+}
+
+void getPS_SetVoltage(int addr) {
+  char *r = getPS_Val(addr, "SV?");
+  set_voltage = int(atof(r) * 100);
+}
+
+void getPS_SetCurrent(int addr) {
+  char *r = getPS_Val(addr, "SI?");
+  set_current = int(atof(r) * 100);
+}
+
+void getPS_Control(int addr) {
+}
+
+void test_PS(){
+
+  getPS_Manuf(ADDRESS);
+  Serial.print("Manuf: ");
+  if (!strlen(manuf)) strcpy(manuf, "UNKWN");
+  Serial.println(manuf);
+  delay(MYDELAY);
+
+  getPS_Model(ADDRESS);
+  Serial.print("Model: ");
+  if (!strlen(model)) strcpy(manuf, "UNKWN");
+  Serial.println(model);
+  delay(MYDELAY);
+
+  getPS_VoltageString(ADDRESS);
+  Serial.print("VoltageSt: ");
+  if (!strlen(voltage_string)) strcpy(manuf, "UNKWN");
+  Serial.println(voltage_string);
+  delay(MYDELAY);
+
+  getPS_Revision(ADDRESS);
+  Serial.print("Rev: ");
+  if (!strlen(revision)) strcpy(manuf, "UNKWN");
+  Serial.println(revision);
+  delay(MYDELAY);
+
+  getPS_ManufDate(ADDRESS);
+  Serial.print("ManufDate: ");
+  if (!strlen(manuf_date)) strcpy(manuf, "UNKWN");
+  Serial.println(manuf_date);
+  delay(MYDELAY);
+
+  getPS_Serial(ADDRESS);
+  Serial.print("Serial: ");
+  if (!strlen(serial)) strcpy(manuf, "UNKWN");
+  Serial.println(serial);
+  delay(MYDELAY);
+
+  getPS_Country(ADDRESS);
+  Serial.print("Country: ");
+  if (!strlen(country)) strcpy(manuf, "UNKWN");
+  Serial.println(country);
+  delay(MYDELAY);
+
+  getPS_RateVoltage(ADDRESS);
+  Serial.print("RateVoltage: ");
+  if (rate_voltage < 0) Serial.println("UNKWN");
+  else Serial.println(rate_voltage);
+  delay(MYDELAY);
+
+  getPS_RateCurrent(ADDRESS);
+  Serial.print("RateCurrent: ");
+  if (rate_current < 0) Serial.println("UNKWN");
+  else Serial.println(rate_current);
+  delay(MYDELAY);
+
+  getPS_MaxVoltage(ADDRESS);
+  Serial.print("MaxVoltage: ");
+  if (max_voltage < 0) Serial.println("UNKWN");
+  else Serial.println(max_voltage);
+  delay(MYDELAY);
+
+  getPS_MaxCurrent(ADDRESS);
+  Serial.print("MaxCurrent: ");
+  if (max_current < 0) Serial.println("UNKWN");
+  else Serial.println(max_current);
+  delay(MYDELAY);
+
+//  snprintf(packetBuffer, sizeof packetBuffer, "{ \"Manufacturer\": \"%s\", \"Model\": \"%s\", \"VoltString\": \"%s\", \"Revision\": \"%s\", \"Serial\": \"%s\", \"VoltageRating\": %d, \"CurrentRating\": %d, \"MaxVoltage\": %d, \"MaxCurrent\": %d}", manuf, model, voltage_string, revision, serial, rate_voltage, rate_current, max_voltage, max_current);
+//  sendMsg(packetBuffer);
+
+  if (setPS_OnOff(ADDRESS, "ON")) Serial.println("Turned it on");
+  else Serial.println("failed to turn it on");
+
+  if (setPS_Voltage(ADDRESS, 1000)) Serial.println("Set volts to 5.0");
+  else Serial.println("failed to set volts");
+
+  if (setPS_Current(ADDRESS, 0)) Serial.println("Set current to 5");
+  else Serial.println("failed to set current");
+
+
+}
 
 void setup() {
+  serial1Buffer.reserve(256);
+
   Serial.begin(BAUD_RATE);
   Serial.println();
   Serial.print(PROG_NAME);
   Serial.println(VERSION);
   Serial.print("Compiled at: ");
   Serial.println(F(__DATE__ " " __TIME__) ); //compile date that is used for a unique identifier
+
+  Serial1.begin(4800);
+  while (!Serial1);
 
   pinMode(ETHERNET_CS, OUTPUT);    // make sure that the default chip select pin is set to output, even if you don't use it:
   pinMode(4, OUTPUT);      // On the Ethernet Shield, CS is pin 4
@@ -291,6 +597,8 @@ void setup() {
   Ethernet.init(ETHERNET_CS);  // Most Arduino shields
   setupBacklights(); //Setup the neopixels
   setupu8g2(); //Setup the graphics display
+
+  test_PS();
   delay(1000);  // Hold the splash screen a second
 }//End setup()
 
