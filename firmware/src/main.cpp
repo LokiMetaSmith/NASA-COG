8505ae08dd067aa0b8e0b34acc10d94bf31c42f6
Robert L. Read <read.robert@gmail.com>
2024-01-23 14:54:27 -0600

    adding debug code to tryp to trap error

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -17,7 +17,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // Program information
 #define COMPANY_NAME "pubinv.org "
 #define PROG_NAME "OEDCS"
-#define VERSION "; Rev: 0.3.21"  // Changed network error code
+#define VERSION "; Rev: 0.3.22"  // Adding some debugging code
 #define DEVICE_UNDER_TEST "Hardware: Due"  //A model number
 #define LICENSE "GNU Affero General Public License, version 3 "
 
