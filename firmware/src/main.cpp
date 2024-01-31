2e3463b4df7194650db9b4bd3bd1de37844d06c7
Robert L. Read <read.robert@gmail.com>
2024-01-31 14:42:41 -0600

    Update main.cpp

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -17,7 +17,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // Program information
 #define COMPANY_NAME "pubinv.org "
 #define PROG_NAME "OEDCS"
-#define OEDCS_VERSION "; Rev: 0.3.39"  // removing debugging statements
+#define OEDCS_VERSION "; Rev: 0.3.40"  // removing debugging statements
 #define DEVICE_UNDER_TEST "Hardware: Due"  // A model number
 #define LICENSE "GNU Affero General Public License, version 3 "
 
