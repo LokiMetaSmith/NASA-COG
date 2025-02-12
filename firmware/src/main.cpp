87ac064c4efbfd61e67b3d3a802c37300ef60e59
Robert L. Read <read.robert@gmail.com>
2025-02-12 00:42:59 -0600

    update version

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -17,7 +17,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // Program information
 #define COMPANY_NAME "pubinv.org "
 #define PROG_NAME "OEDCS"
-#define OEDCS_VERSION "; Rev: 0.4.9"  // Fixing Cooldown bug
+#define OEDCS_VERSION "; Rev: 0.4.10"  // Improving Thermocouple Handling
 #define DEVICE_UNDER_TEST "Hardware: Due"  // A model number
 #define LICENSE "GNU Affero General Public License, version 3 "
 
