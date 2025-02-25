232b486827e42fc523b0a810257d7c41d03ce02c
lokimetasmith <lokimail@gmail.com>
2025-02-25 13:14:45 -0600

    added some server protection and security features
    Signed-off-by: lokimetasmith <lokimail@gmail.com>

--- SBC setup instructions.sh
+++ SBC setup instructions.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #download  Ubuntu Server 22.04.3 LTS                                                       
 #copy to usb                                                                     
 #plug in keyboard , display and usb to sbc
@@ -47,9 +49,25 @@ sudo ln -s ~/.platformio/penv/bin/platformio /usr/local/bin/platformio
 sudo ln -s ~/.platformio/penv/bin/pio /usr/local/bin/pio
 sudo ln -s ~/.platformio/penv/bin/piodebuggdb /usr/local/bin/piodebuggdb
 
+sudo apt install isc-dhcp-server sed
+
+echo -e "default-lease-time 600;\nmax-lease-time 7200;\noption subnet-mask 255.255.255.0;\noption broadcast-address 192.168.1.255;\noption routers 192.168.1.254;\noption domain-name-servers 192.168.1.1, 192.168.1.2;\noption domain-name \"mydomain.example\";\nsubnet 192.168.1.0 netmask 255.255.255.0 { range 192.168.1.10 192.168.1.100; range 192.168.1.150 192.168.1.200; }\n" > /etc/dhcp3/dhcpd.conf
+echo -e "INTERFACES="eth1" > /etc/default/dhcp3-server
+
+echo -e "
+
 #install the local mcogserver
 #sudo apt-get install libssl-dev
 #git clone https://github.com/PubInv/mcogserver.git
 #make iotserver
 #sudo systemctl start mcogs.service
 #sudo systemctl enable mcogs.service
+
+#setup ssh
+sudo apt install ssh fail2ban
+cd /etc/fail2ban
+sudo cp jail.conf jail.local
+head -20 jail.conf
+sudo ufw allow 22      # if you have ufw running, but it doesn't hurt to run.
+#while plugged into the local port
+#ssh-copy-id user@10.0.0.1
