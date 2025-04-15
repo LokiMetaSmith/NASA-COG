9ffe69036f23684b5c1196fca069936b3c8bc4e0
Robert L. Read <read.robert@gmail.com>
2025-04-15 07:57:05 -0700

    configure for Blue

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -2,7 +2,7 @@
 // Program information
 #define COMPANY_NAME "pubinv.org "
 #define PROG_NAME "OEDCS"
-#define OEDCS_VERSION "; Rev: 0.4.14"  // Further improvements in OFF state
+#define OEDCS_VERSION "; Rev: 0.4.15"  // Further improvements in OFF state
 #define DEVICE_UNDER_TEST "Hardware: Due"  // A model number
 #define LICENSE "GNU Affero General Public License, version 3 "
 
@@ -59,8 +59,8 @@ ShutdownButtonTask shutdownButtonTask;
 MachineConfig machineConfig;
 /***********************************/
 
-#define ETHERNET_REQUIRED 1
-// #define ETHERNET_REQUIRED 0 //No ethernet.
+// #define ETHERNET_REQUIRED 1
+#define ETHERNET_REQUIRED 0 //No ethernet.
 
 
 // This is to allow a code idiom compatible with the way
